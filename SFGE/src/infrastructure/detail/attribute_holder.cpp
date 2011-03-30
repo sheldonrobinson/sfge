816a64574fb73693db1690d465e4bb545b913a61
Denys <Denys@localhost>
2011-03-30 22:57:02 +0200

    Now allocating enought space to actually hold an attribute

--- SFGE/src/infrastructure/detail/attribute_holder.cpp
+++ SFGE/src/infrastructure/detail/attribute_holder.cpp
@@ -1,5 +1,7 @@
 #include "sfge/infrastructure/detail/attribute_holder.hpp"
 
+#include <cstdlib>
+
 namespace sfge
 {
 namespace detail
@@ -8,12 +10,15 @@ namespace detail
 const AttributeHolderPtr AttributeHolder::InvalidHolderPtr(0);
 
 AttributeHolder::AttributeHolder(const TypeRegistry::TypeInfo &typeInfo)
-	: mTypeInfo(typeInfo)
+	: mTypeInfo(typeInfo), mValue(0)
 {
+	if (mTypeInfo.second > sizeof(void*))
+		mValue = malloc(mTypeInfo.second);
 }
 
 AttributeHolder::~AttributeHolder()
 {
+	free(mValue);
 }
 
 TypeRegistry::TypeId AttributeHolder::GetTypeId() const
