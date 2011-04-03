584d9c296f17658f4f53f09e4cd5b8d9dba49012
Denys <Denys@localhost>
2011-04-03 21:25:11 +0200

    Simplified BasicGameStructure sample to only have one object (and actually be *really* basic)

--- samples/sampleBasicGameStructure/main.cpp
+++ samples/sampleBasicGameStructure/main.cpp
@@ -3,11 +3,8 @@
 #include <sfge/infrastructure/game_object.hpp>
 #include <sfge/graphics/graphic_system.hpp>
 
-#include "orbiter_behaviour.hpp"
 #include "controller_chng_pos_color_behaviour.hpp"
 
-using namespace std;
-using namespace boost;
 using namespace sf;
 using namespace sfge;
 
@@ -20,7 +17,6 @@ protected:
 	virtual void OnDeclareAdditionnalBehaviours() override
 	{
 		DECLARE_BEHAVIOUR(ControllerBehaviour);
-		DECLARE_BEHAVIOUR(OrbiterBehaviour);
 	}
 
 	virtual void OnEndSystemInit() override
@@ -48,36 +44,15 @@ private:
 		refObjDrawableDef.put("cb",		Color::Magenta.b);
 		refObjDrawableDef.put("ca",		Color::Magenta.a);
 
-		Parameters orbitObjDrawableDef;
-		orbitObjDrawableDef.put("type",		"shape");
-		orbitObjDrawableDef.put("shape",	"circle");
-		orbitObjDrawableDef.put("cx",		0);
-		orbitObjDrawableDef.put("cy",		0);
-		orbitObjDrawableDef.put("radius",	15);
-		orbitObjDrawableDef.put("cr",		Color::White.r);
-		orbitObjDrawableDef.put("cg",		Color::White.g);
-		orbitObjDrawableDef.put("cb",		Color::White.b);
-		orbitObjDrawableDef.put("ca",		Color::White.a);
-		
-		Parameters orbitParams;
-		orbitParams.put("distance",			100.f);
-		orbitParams.put("speed",			5.f);
-		orbitParams.put("revolutionCenter",	"refObj");
-
 		// Declare the 2 gameobject definitions we need
 		DataStore &ds = DataStore::getSingleton();
 
 		ds.DeclareGameObjectDef("ControllableThing");
-		ds.DeclareGameObjectDef("OrbitingThing");
 
 		// Create the links between gameobject definitions and registered behaviours (eventually settings some default params)
 		ds.LinkBehaviourDefToGameObjectDef("ControllableThing", "TransformBehaviour");
 		ds.LinkBehaviourDefToGameObjectDef("ControllableThing", "ControllerBehaviour");
 		ds.LinkBehaviourDefToGameObjectDef("ControllableThing", "RenderBehaviour",	refObjDrawableDef);
-		
-		ds.LinkBehaviourDefToGameObjectDef("OrbitingThing", "TransformBehaviour");
-		ds.LinkBehaviourDefToGameObjectDef("OrbitingThing", "OrbiterBehaviour",		orbitParams);
-		ds.LinkBehaviourDefToGameObjectDef("OrbitingThing", "RenderBehaviour",		orbitObjDrawableDef);
 	}
 
 	void SetupScene()
@@ -86,9 +61,6 @@ private:
 
 		GameObjectPtr refGO = ds.InstantiateGameObjectDef("ControllableThing", "refObj");
 		mObjects.push_back(refGO);
-
-		GameObjectPtr orbiterGO = ds.InstantiateGameObjectDef("OrbitingThing");
-		mObjects.push_back(orbiterGO);
 
 		ds.InitializeInstances();
 	}
